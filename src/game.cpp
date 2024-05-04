de6138c60f5b2a83b2ea2ce089c27bac69c93870
Isbah Malik <129406085+Isbah-23@users.noreply.github.com>
2024-05-04 18:59:01 +0500

    committing to pull

--- src/game.cpp
+++ src/game.cpp
@@ -856,7 +856,7 @@ void Game::testRun()
     smg.setVolume(0);
     window.setFramerateLimit(0);
     int* character = nullptr;
-    int idek[2] = {10,10}; //set character and enemy index from here for faster debugging/testing(no so fast when you have to look integers) - remember em then
+    int idek[2] = {11,11}; //set character and enemy index from here for faster debugging/testing(no so fast when you have to look integers) - remember em then
     int* set = idek;
     setStage(set);
     smg.play(vs_music);
